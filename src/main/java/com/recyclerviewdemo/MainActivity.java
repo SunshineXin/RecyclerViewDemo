eed0f92e198a9db042b8d5c6c36cfbb95d753146
liyang <yangli@youku.com>
2015-08-20 18:02:27 +0800

    加入滑动事件的监听

--- src/main/java/com/recyclerviewdemo/MainActivity.java
+++ src/main/java/com/recyclerviewdemo/MainActivity.java
@@ -6,25 +6,36 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.LinearLayout;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.List;
+
+import static android.support.v7.widget.RecyclerView.SCROLL_STATE_DRAGGING;
+import static android.support.v7.widget.RecyclerView.SCROLL_STATE_IDLE;
+import static android.support.v7.widget.RecyclerView.SCROLL_STATE_SETTLING;
 
 public class MainActivity extends AppCompatActivity implements SwipeRefreshLayout.OnRefreshListener{
     private SwipeRefreshLayout mSwipeLayout;
     private RecyclerView mRecyclerView;
 
     private RecyclerViewAdapter mAdapter;
+    private LinearLayoutManager mLayoutManager;
+    private TextView mPositionTV, mCountTV, mStateTV;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mPositionTV = (TextView) findViewById(R.id.position);
+        mCountTV = (TextView) findViewById(R.id.count);
+        mStateTV = (TextView) findViewById(R.id.state);
 
         mSwipeLayout = (SwipeRefreshLayout) findViewById(R.id.swipeRefreshLayout);
         mSwipeLayout.setOnRefreshListener(this);
@@ -33,13 +44,13 @@ public class MainActivity extends AppCompatActivity implements SwipeRefreshLayou
 //                android.R.color.holo_orange_light,
 //                android.R.color.holo_red_light);
 
-        mAdapter =  new RecyclerViewAdapter(getData(10));
+        mAdapter =  new RecyclerViewAdapter(getData(40));
         mRecyclerView = (RecyclerView) findViewById(R.id.recyclerView);
         // 创建一个线性布局管理器
-        LinearLayoutManager layoutManager = new LinearLayoutManager(this);
-//        layoutManager.setOrientation(LinearLayout.HORIZONTAL);//将item设置为横向
+        mLayoutManager = new LinearLayoutManager(this);
+//        mLayoutManager.setOrientation(LinearLayout.HORIZONTAL);//将item设置为横向
         // 设置布局管理器
-        mRecyclerView.setLayoutManager(layoutManager);
+        mRecyclerView.setLayoutManager(mLayoutManager);
         mAdapter.setOnItemClickListener(new RecyclerViewAdapter.OnItemClikListener() {
             @Override
             public void onItemClick(View view, int position) {
@@ -54,6 +65,17 @@ public class MainActivity extends AppCompatActivity implements SwipeRefreshLayou
         });
         mRecyclerView.setAdapter(mAdapter);
 
+        mRecyclerView.addOnScrollListener(new RecyclerView.OnScrollListener() {
+            @Override
+            public void onScrollStateChanged(RecyclerView recyclerView, int newState) {
+                super.onScrollStateChanged(recyclerView, newState);
+                mPositionTV.setText("First:" + mLayoutManager.findFirstVisibleItemPosition());
+                mCountTV.setText("Counts:" + mRecyclerView.getChildCount());
+                updateState(newState);
+            }
+        });
+
+//        mRecyclerView.setItemAnimator();
     }
 
     private List<String> getData(int count){
@@ -68,6 +90,24 @@ public class MainActivity extends AppCompatActivity implements SwipeRefreshLayou
         return list;
     }
 
+    private void updateState(int scrollState) {
+        String stateName = "Undefined";
+        switch(scrollState) {
+            case SCROLL_STATE_IDLE:
+                stateName = "Idle";
+                break;
+
+            case SCROLL_STATE_DRAGGING:
+                stateName = "Dragging";
+                break;
+
+            case SCROLL_STATE_SETTLING:
+                stateName = "Flinging";
+                break;
+        }
+
+        mStateTV.setText(stateName);
+    }
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
